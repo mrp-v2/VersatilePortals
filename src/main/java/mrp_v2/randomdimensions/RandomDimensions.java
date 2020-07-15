86ab0d319390f18bee483663b9c7e943ecc251e0
mrp-v2 <matthrewp@gmail.com>
2020-07-15 15:42:55 -0400

    created mod class

---
+++ src/main/java/mrp_v2/randomdimensions/RandomDimensions.java
@@ -0,0 +1,13 @@
+package mrp_v2.randomdimensions;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RandomDimensions.ID)
+public class RandomDimensions {
+
+	public static final String ID = "random" + "dimensions";
+
+	public RandomDimensions() {
+
+	}
+}
