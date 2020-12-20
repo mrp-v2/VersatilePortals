bdfd2351c2e724305e69a0198db52e1cf2df24e1
mrp-v2 <matthrewp@gmail.com>
2020-12-20 12:10:01 -0500

    update mrp Library version

--- src/main/java/mrp_v2/versatileportals/datagen/DataGenHandler.java
+++ src/main/java/mrp_v2/versatileportals/datagen/DataGenHandler.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
         if (event.includeServer())
         {
             helper.addLootTables(new LootTables());
-            helper.addRecipeGenerator(RecipeGenerator::new);
+            helper.addRecipeProvider(RecipeGenerator::new);
         }
         if (event.includeClient())
         {
